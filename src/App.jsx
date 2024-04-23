9b41a789bb241bd9f5a9b853e7cb45c41fddcc78
Md Shamim <150214216+mdshamim125@users.noreply.github.com>
2024-04-23 13:01:40 +0600

    login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,59 @@
-
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { FaEyeSlash, FaEye } from "react-icons/fa";
 
 function App() {
-
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <>
-      <div>This is Login page</div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col">
+          <div className="text-center lg:text-left">
+            <h1 className="lg:text-5xl md:text-4xl text-4xl font-bold mb-4">Admin Login</h1>
+            
+          </div>
+          <div className="card flex-shrink-0 lg:w-[600px] md:w-[500px] w-[300px] mt-6 shadow-2xl">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <div className="relative w-full">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    name="password"
+                    placeholder="password"
+                    className="input input-bordered w-full"
+                    required
+                  />
+                  <span
+                    className="absolute top-3 right-4 w-[]"
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? <FaEyeSlash /> : <FaEye />}
+                  </span>
+                </div>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary font-bold">Login</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
